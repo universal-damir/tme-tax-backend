dbd46cc4515d24c2eadb5ab54d03a793a8f32562
universal-damir <64892195+universal-damir@users.noreply.github.com>
2025-02-13 16:42:06 +0400

    o3 fix.22

--- server.mjs
+++ server.mjs
@@ -194,6 +194,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
